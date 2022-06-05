fbecf4bea50d9e4df0b7c1c2b9862941917a1fa1
quuer <5409600@qq.com>
2022-06-05 15:22:40 +0800

    feat:新增rematch/model支持

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App() {
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import styles from './app.less'
+
+function App(props) {
+  console.log('props---||', props)
+  const { a, dispatch } = props
+  const handleAsync = () => {
+    console.log('异步---||', a)
+    dispatch({ type: 'index/incrementAsync', payload: { a: 1 } })
+
+  }
+  const handleSync = () => {
+    console.log('同步---||', a)
+    dispatch({ type: 'index/updateState', payload: { a: 1 } })
+  }
   return (
-    <div className="App">
-      app
+    <div className={styles.App}>
+      <button onClick={handleAsync}>dispatch异步</button>
+      <button onClick={handleSync}>dispatch同步</button>
     </div>
   )
 }
-
-export default App
+const mapState = ({ global }) => {
+  return global
+}
+const mapDispatch = (dispatch) => {
+  return {
+    dispatch
+  }
+}
+// export default connect(mapState,mapDispatch)(App) 或下面的写法
+export default connect(({ global }) => global, dispatch => ({ dispatch }))(App)
---
+++ src/models/a.js
@@ -0,0 +1,19 @@
+const global = {
+  name: 'global',
+  state: {
+    a: 5
+  },
+  reducers: {
+    setState(state, newState) {
+      return { ...state, ...newState }
+    }
+  },
+  effects: (dispatch) => ({
+    async incrementAsync(payload) {
+      console.log('payload', payload)
+      // dispatch.index.setState({ a: 2 }) 或下面一种写法
+      dispatch({ type: 'index/setState', payload:{a:1} }) // 推荐写法,payload为固定写法
+    }
+  })
+}
+export default global
